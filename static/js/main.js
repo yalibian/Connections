f709cfecf7b4e52ebd0923a636d806cbfa83f903
Yali Bian <Yali.Bian@hotmail.com>
2017-05-20 16:10:35 -0400

    Update html

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,14 @@
  * Created by Yali on 5/18/17.
  */
 
-console.log('hello world');
+// console.log('hello world');
+
+function submit(){
+	console.log('In submit entity');
+	let entity = document.getElementById("entity").value;
+	let summary = document.getElementById("summary").value;
+	let hypo = document.getElementById("hypo").value;
+	console.log(entity);
+	console.log(summary);
+	console.log(hypo);
+}
